fda36d5917e13971a70f0a0250ae49632a08d3e6
Qiji <99153661+dq-qiji@users.noreply.github.com>
2022-10-06 14:06:34 +0700

    [dq-qiji] Area API: Repository

---
+++ Services/AreaService.cs
@@ -0,0 +1,53 @@
+﻿using BusinessObject.Models;
+using Repositories.IRepositories;
+using Services.IServices;
+using Services.ValidationHandling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class AreaService : IAreaService
+    {
+        private readonly IAreaRepository _areaRepository;
+        private readonly AreaValidation _areaValidation;
+
+        public AreaService(IAreaRepository areaRepository, AreaValidation areaValidation)
+        {
+            _areaRepository = areaRepository;
+            _areaValidation = areaValidation;
+        }
+        public Task<Response<Area>> GetReponseAreaById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<List<Area>>> GetReponseAreas(int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Area>> GetReponseChangeStatusArea(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Area>> GetReponseUpdateArea(string id, Area area)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<int>> GetResponseAreaNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Area>> GetResponseCreateAnArea(Area area)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/ValidationHandling/AreaValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ValidationHandling
+{
+    public class AreaValidation
+    {
+        public int ValidationId(string id)
+        {
+            try
+            {
+                int _id = int.Parse(id);
+                return _id;
+            }
+            catch (FormatException)
+            {
+                return -1;
+            }
+        }
+    }
+}
