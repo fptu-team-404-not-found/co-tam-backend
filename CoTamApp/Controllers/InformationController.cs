521d54f05eddfb4b452c41e40a41943605a2e7af
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2022-10-11 17:36:46 +0700

    Dat: Add Full Information Api

---
+++ CoTamApp/Controllers/InformationController.cs
@@ -0,0 +1,81 @@
+﻿using BusinessObject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using Services.IServices;
+
+namespace CoTamApp.Controllers
+{
+    [Route("api/informations")]
+    [ApiController]
+    public class InformationController : ControllerBase
+    {
+        private readonly IInformationService _informationService;
+
+        public InformationController(IInformationService informationService)
+        {
+            _informationService = informationService;
+        }
+        [HttpGet("count")]
+        public async Task<ActionResult<Response<string>>> CountInformation()
+        {
+            var res = await _informationService.CountInformation();
+            if (!res.Success)
+                return BadRequest(res);
+            return Ok(res);
+        }
+        [HttpGet]
+        public async Task<ActionResult<Response<List<Information>>>> GetListInformationWithPagination([FromBody] Pagination pagination)
+        {
+            var res = await _informationService.GetAllInformationWithPagination(pagination.PageIndex, pagination.PageSize);
+            if (!res.Success)
+                return BadRequest(res);
+            return Ok(res);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Response<Information>>> GetInformationById(int id)
+        {
+            var res = await _informationService.GetInformationById(id);
+            if (!res.Success)
+                return BadRequest(res);
+            return Ok(res);
+        }
+        /// <summary>
+        /// Add New Information with information include Name, Description
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Description: 
+        /// - Name is required when create
+        /// </remarks>
+        [HttpPost]
+        public async Task<ActionResult<Response<string>>> CreateNewInformation(Information information)
+        {
+            var res = await _informationService.CreateNewInformation(information);
+            if (!res.Success)
+                return BadRequest(res);
+            return Ok(res);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Response<string>>> DisableOrEnableInformation(int id)
+        {
+            var res = await _informationService.DisableOrEnableInformation(id);
+            if (!res.Success)
+                return BadRequest(res);
+            return Ok(res);
+        }
+        [HttpPut]
+        public async Task<ActionResult<Response<Information>>> UpdatePromotion(Information information)
+        {
+            try
+            {
+                var response = await _informationService.UpdateInformation(information);
+                return StatusCode((int)response.StatusCode, response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Services/ValidationHandling/InformationValidation.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ValidationHandling
+{
+    public class InformationValidation
+    {
+        public string CheckInformationValidation(Information information)
+        {
+            if (string.IsNullOrEmpty(information.Name) || string.IsNullOrEmpty(information.Discription))
+                return "Hãy nhập name, description của information";
+            return "ok";
+        }
+    }
+}
